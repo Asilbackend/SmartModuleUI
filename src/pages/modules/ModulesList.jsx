c17483e53a35cade58c4fca856eab51744b73591
faxriddin <faspace2040@gmail.com>
2025-07-17 20:32:57 +0500

    updated

--- src/pages/modules/ModulesList.jsx
+++ src/pages/modules/ModulesList.jsx
@@ -40,8 +40,8 @@ export default function ModuleList() {
   };
 
   return (
-    <div className='px-8 py-6'>
-      <div className='mb-6 rounded-2xl bg-white p-6 shadow-sm'>
+    <div className='py-3 sm:px-4 sm:py-6 md:px-8'>
+      <div className='mb-4 rounded-2xl bg-white p-4 shadow-sm sm:mb-6 sm:p-6'>
         <h2 className='mb-4 text-2xl font-semibold'>Modullar video va matnlardan iborat.</h2>
         <p className='text-gray-700'>
           Har bir modulda bo‘limlar, bo‘limlarda esa darslar ketma-ketlikda.
@@ -49,7 +49,7 @@ export default function ModuleList() {
       </div>
 
       {mod.sections.map((section) => (
-        <div key={section.id} className='mb-8 rounded-2xl bg-white p-6 shadow-sm'>
+        <div key={section.id} className='mb-8 rounded-2xl bg-white p-4 shadow-sm sm:p-6'>
           <h2 className='mb-4 text-2xl font-semibold text-blue-900'>
             {section.id}. {section.title}
           </h2>
@@ -58,20 +58,20 @@ export default function ModuleList() {
             direction='vertical'
             current={currentSteps[section.id]}
             items={section.items.map((item, index) => ({
-              title: item.title,
+              // title: item.title,
               description: (
-                <div className='flex items-center justify-between'>
-                  <div className='flex gap-2 text-gray-500'>
-                    <p className='text-sm'>{item.type}</p>
-                    <p className='text-sm'>{item.duration}</p>
+                <div className='mb-2 flex items-center justify-between gap-1'>
+                  <div className='flex flex-col'>
+                    <h3 className='text-[16px]'>{item.title}</h3>
+                    <div className='flex gap-2 text-gray-500'>
+                      <p className='text-sm'>{item.type}</p>
+                      <p className='text-sm'>{item.duration}</p>
+                    </div>
                   </div>
-                  {/* 🔑 Qoidani shu yerda tekshiramiz:
-                        - Agar step index <= current => tugma ko‘rinadi
-                        - Faqat keyingi step tugmasi ko‘rinmaydi */}
                   {index <= currentSteps[section.id] && (
                     <button
                       onClick={() => handleStepClick(section.id, index, item)}
-                      className='rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700'
+                      className='rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700'
                     >
                       Ko‘rish
                     </button>
--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -35,7 +35,6 @@ const ProfilePage = () => {
   ];
 
   const rightInfo = [
-    { label: 'Guruh', value: student.guruh },
     { label: 'Darajasi', value: student.daraja },
     { label: 'Ta’lim shakli', value: student.talimShakli },
     { label: 'Stipendiya', value: student.stipendiya },
@@ -44,18 +43,19 @@ const ProfilePage = () => {
   return (
     <div className='mx-auto max-w-6xl'>
       <div className='gap-4 sm:gap-6 md:gap-8'>
-        <div className='grid grid-cols-12 gap-6 md:col-span-6 lg:col-span-5'>
-          <div className='col-span-5 rounded-3xl bg-white shadow-xs'>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-12'>
+          {/* Video Block */}
+          <div className='rounded-3xl bg-white shadow-xs lg:col-span-5'>
             <video
               controls
               poster='/news-image.png'
-              className='mx-auto mb-4 h-[280px] w-full rounded-3xl object-cover'
+              className='mx-auto mb-4 h-auto w-full rounded-3xl object-cover md:h-[280px]'
             >
               <source src='./123.mp4' type='video/mp4' />
               Sizning browser videoni qo‘llab-quvvatlamaydi.
             </video>
             <div className='px-4 pb-4'>
-              <h3 className='mb-2 text-2xl font-bold text-[#013464]'>
+              <h3 className='mb-2 text-xl font-bold text-[#013464] md:text-2xl'>
                 Digital Science jurnali endi TATUda
               </h3>
               <p>
@@ -64,27 +64,29 @@ const ProfilePage = () => {
               </p>
             </div>
           </div>
-          <div className='col-span-7 rounded-3xl bg-white p-4 shadow-xs'>
-            <div className='flex max-w-3xl flex-col rounded-lg md:flex-row'>
-              <div className='flex items-center justify-center p-4 md:w-1/3'>
+
+          {/* Info Block */}
+          <div className='rounded-3xl bg-white p-2 shadow-xs lg:col-span-7'>
+            <div className='flex flex-col rounded-lg md:flex-row'>
+              <div className='flex items-center justify-center p-2 sm:p-4 md:w-1/3'>
                 <img
                   src='./image.png'
                   alt='Talaba rasmi'
-                  className='h-60 w-full rounded-lg object-cover'
+                  className='w-full rounded-lg object-cover md:h-60'
                 />
               </div>
-              <div className='p-6 md:w-2/3'>
-                <div className='grid grid-cols-1 md:grid-cols-2'>
-                  <div className='border-r pr-2'>
+              <div className='p-2 md:w-2/3'>
+                <div className='grid grid-cols-1 sm:gap-4 md:grid-cols-2'>
+                  <div className='md:border-r md:pr-4'>
                     {leftInfo.map((item) => (
-                      <p className='py-1' key={item.label}>
+                      <p key={item.label} className='py-1 md:text-base'>
                         <span className='font-semibold'>{item.label}:</span> {item.value}
                       </p>
                     ))}
                   </div>
-                  <div className='pl-3'>
+                  <div className='md:pl-4'>
                     {rightInfo.map((item) => (
-                      <p key={item.label} className='py-1'>
+                      <p key={item.label} className='py-1 md:text-base'>
                         <span className='font-semibold'>{item.label}:</span> {item.value}
                       </p>
                     ))}
@@ -96,15 +98,18 @@ const ProfilePage = () => {
         </div>
 
         <div className='col-span-12 md:col-span-6 lg:col-span-7'>
+          {/* Card 1 */}
           <div className='mt-4 rounded-2xl bg-white px-4 pt-4 pb-6 shadow-xs md:mt-8'>
-            <div className='mb-8 flex'>
-              <div>
-                <h2 className='mb-2 text-2xl font-semibold'>
+            <div className='mb-8 flex flex-col gap-4 md:flex-row md:items-start'>
+              <div className='flex-1'>
+                <h2 className='mb-2 text-xl font-semibold md:text-2xl'>
                   1. Universitet tarixi va tashkiliy tuzilmasi (Required)
                 </h2>
-                <p className='text-lg'>Universitetning istiqbolli yo’nalishlari (Matn)</p>
+                <p className='text-base md:text-lg'>
+                  Universitetning istiqbolli yo’nalishlari (Matn)
+                </p>
               </div>
-              <div className='ml-auto flex flex-col items-end gap-2'>
+              <div className='flex flex-col items-start gap-2 md:ml-auto md:items-end'>
                 <Button
                   type='primary'
                   icon={<Check strokeWidth={1.5} />}
@@ -121,7 +126,7 @@ const ProfilePage = () => {
                   size='large'
                   iconPosition='end'
                 >
-                  Sertifikatni ko`rish
+                  Sertifikatni ko‘rish
                 </Button>
               </div>
             </div>
@@ -129,21 +134,25 @@ const ProfilePage = () => {
               <Progress
                 percent={60}
                 percentPosition={{ align: 'center', type: 'inner' }}
-                style={{ width: '100%', height: '40%' }}
+                style={{ width: '100%' }}
                 strokeWidth={20}
                 strokeColor='#008CFF'
               />
             </Flex>
           </div>
+
+          {/* Card 2 */}
           <div className='mt-4 rounded-2xl bg-white px-4 pt-4 pb-6 shadow-xs md:mt-8'>
-            <div className='mb-8 flex'>
-              <div>
-                <h2 className='mb-2 text-2xl font-semibold'>
-                  1. Universitet tarixi va tashkiliy tuzilmasi (Required)
+            <div className='mb-8 flex flex-col gap-4 md:flex-row md:items-start'>
+              <div className='flex-1'>
+                <h2 className='mb-2 text-xl font-semibold md:text-2xl'>
+                  2. Universitet tarixi va tashkiliy tuzilmasi (Required)
                 </h2>
-                <p className='text-lg'>Universitetning istiqbolli yo’nalishlari (Matn)</p>
+                <p className='text-base md:text-lg'>
+                  Universitetning istiqbolli yo’nalishlari (Matn)
+                </p>
               </div>
-              <div className='ml-auto flex flex-col items-end gap-2'>
+              <div className='flex flex-col items-start gap-2 md:ml-auto md:items-end'>
                 <Button
                   type='primary'
                   icon={<Check strokeWidth={1.5} />}
@@ -160,7 +169,7 @@ const ProfilePage = () => {
                   size='large'
                   iconPosition='end'
                 >
-                  Sertifikatni ko`rish
+                  Sertifikatni ko‘rish
                 </Button>
               </div>
             </div>
@@ -168,7 +177,7 @@ const ProfilePage = () => {
               <Progress
                 percent={80}
                 percentPosition={{ align: 'center', type: 'inner' }}
-                style={{ width: '100%', height: '40%' }}
+                style={{ width: '100%' }}
                 strokeWidth={20}
                 strokeColor='#008CFF'
               />
@@ -181,10 +190,10 @@ const ProfilePage = () => {
             <X
               size={40}
               strokeWidth={1}
-              className='absolute top-20 right-20 cursor-pointer text-white'
+              className='absolute top-10 right-5 cursor-pointer text-white sm:top-20 sm:right-20'
               onClick={handleClose}
             />
-            <img src='/sertifikat.png' className='w-[80%] max-w-4xl select-none' />
+            <img src='/sertifikat.png' className='w-[90%] max-w-4xl select-none sm:w-[80%]' />
           </div>
         )}
       </div>
